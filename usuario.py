5a28e52a83081fbce75e82e1162ce46a1c229d11
Esteban Segura <esteban.1703@gmail.com>
2021-03-23 22:13:36 -0600

    Se define una clase que se encargara de manejar los usuarios creados

---
+++ usuario.py
@@ -0,0 +1,9 @@
+class Usuario(object):
+    def __init__(self,nombre,email,usuario,password):
+        self.nombre = nombre
+        self.email = email
+        self.usuario = usuario
+        self.password = password
+
+    
+    
